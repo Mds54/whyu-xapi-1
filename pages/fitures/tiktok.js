02301afa3c80799425ddfe2de3c344cc78e3c662
M5R4 <xzyytr@gmail.com>
2024-12-21 12:33:47 +0800

    tiktok.js

--- pages/fitures/tiktok.js
+++ pages/fitures/tiktok.js
@@ -4,25 +4,13 @@
    * follow more instagram: @iqstore78
 */
 const axios = require("axios")
-const allowedApiKeys = require("../../declaration/arrayKey.jsx")
 
 module.exports = async (req, res) => {
   const urls = req.query.urls
-  const apiKey = req.query.apiKey
 
   if (!urls) {
     return res.status(400).json({
       error: "Url Tiktok Nya Mana?"
-    })
-  }
-
-  if (!apiKey) {
-    return res.status(403).json({
-      error: "Input Parameter Apikey!"
-    })
-  } else if (!allowedApiKeys.includes(apiKey)) {
-    return res.status(403).json({
-      error: "apikey not found"
     })
   }
 
