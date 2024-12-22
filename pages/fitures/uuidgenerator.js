7217f349707669e38ef97007f0fda230cb9084f4
M5R4 <xzyytr@gmail.com>
2024-12-22 10:21:26 +0800

    Add files via upload

--- pages/fitures/uuidgenerator.js
+++ pages/fitures/uuidgenerator.js
@@ -1,18 +1,19 @@
-const fetch = require("node-fetch")
+/*
+   * by balzz
+   * dont delate my wm
+   * follow more instagram: @iqstore78
+*/
 const axios = require("axios")
 
 module.exports = async (req, res) => {
 
-
-const data = await fetch(`https://www.uuidgenerator.net/api/version7`)
-        .then(response => response.json())
-        .then(data => {
-        var result = data;
-             res.json({
-             	author: 'XzyyMDS',
-                 result
-             })
-         })
+  const url = 'https://www.uuidgenerator.net/api/version7'
+    try {
+    const response = await axios.get(url)
+    const data = response
+    res.status(200).json({
+      data
+    })
   } catch (error) {
     res.status(500).json({
       error: "Ada masalah, coba lagi nanti"
